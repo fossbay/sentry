f453119a00d99b5c8d78eaafc4d187c20e6733b8
miten <57693631+m1ten@users.noreply.github.com>
2024-01-15 10:07:53 -0500

    clean slate

--- sentry-go/main.go
+++ sentry-go/main.go
@@ -2,11 +2,8 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
-	"net/http"
 	"os"
 	"os/signal"
-	"strings"
 	"syscall"
 
 	"github.com/bwmarrin/discordgo"
@@ -65,117 +62,4 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 		latency := s.HeartbeatLatency().Seconds() * 1000
 		s.ChannelMessageSend(m.ChannelID, "Pong! "+fmt.Sprintf("%.f", latency)+"ms")
 	}
-
-	// If the message is "pong" reply with "Ping!"
-	if m.Content == "pong" {
-		s.ChannelMessageSend(m.ChannelID, "Ping!")
-	}
-
-	// clear the chat
-	if m.Content == "clear" {
-		ids := []string{}
-		msgs, _ := s.ChannelMessages(m.ChannelID, 5, "", "", "")
-		for _, msg := range msgs {
-			ids = append(ids, msg.ID)
-		}
-		err := s.ChannelMessagesBulkDelete(m.ChannelID, ids)
-		if err != nil {
-			s.ChannelMessageSend(m.ChannelID, "Error: "+err.Error())
-			return
-		}
-		s.ChannelMessageSend(m.ChannelID, "Chat Cleared!")
-	}
-
-	// Ban Gabry
-	if m.Content == "ban" {
-		s.GuildBanCreate(m.GuildID, "948242943595147325", 0)
-		s.ChannelMessageSend(m.ChannelID, "Gabry Banned!")
-	}
-
-	// Unban Gabry
-	if m.Content == "unban" {
-		s.GuildBanDelete(m.GuildID, "948242943595147325")
-		s.ChannelMessageSend(m.ChannelID, "Gabry Unbanned!")
-	}
-
-	// generate a random number
-	// command: rand <min> <max> <precision>
-	// assume min, max, and result are floats; aka don't cast to int
-	// reply: min:<min> and max:<max> -> <result>
-	if m.Content == "rand" {
-		s.ChannelMessageSend(m.ChannelID, "Usage: rand <min> <max> <precision>")
-	}
-	if len(m.Content) > 5 && m.Content[:4] == "rand" {
-		var min, max float64
-		var precision int
-		_, err := fmt.Sscanf(m.Content, "rand %f %f %d", &min, &max, &precision)
-		if err != nil {
-			s.ChannelMessageSend(m.ChannelID, "Error: "+err.Error())
-			return
-		}
-		result := rand.Float64()*(max-min) + min
-		s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("min:%f and max:%f -> %.*f", min, max, precision, result))
-	}
-
-	// say command (message is the rest of the string)
-	// command: say <target> <message>
-	// reply: Embed with message and target
-	if m.Content == "say" {
-		s.ChannelMessageSend(m.ChannelID, "Usage: say <target> <message>")
-	}
-	if len(m.Content) > 4 && m.Content[:3] == "say" {
-		target, message := "", ""
-		parts := strings.SplitN(m.Content, " ", 3)
-		if len(parts) == 3 {
-			target = parts[1]
-			message = parts[2]
-		}
-
-		s.ChannelMessageSendEmbed(m.ChannelID, &discordgo.MessageEmbed{
-			Title:       "Message from " + m.Author.Username,
-			Description: message,
-			Footer: &discordgo.MessageEmbedFooter{
-				Text: "To " + target,
-			},
-		})
-	}
-
-	// nice command
-	// command: nice
-	// reply: no, you're nice :)
-	if m.Content == "nice" {
-		// if author is owner of bot, reply with "no, you're nice :)"
-		if m.Author.ID == OwnerID {
-			s.ChannelMessageSend(m.ChannelID, "no, you're nice :)")
-		} else {
-			s.ChannelMessageSend(m.ChannelID, "haha, no")
-		}
-	}
-
-	// check if user is rich or poor (based on nitro/premium)
-	// command: wealth
-	// reply: "You are rich" or "You are poor"
-	if m.Content == "wealth" {
-		if m.Author.PremiumType == 2 || m.Author.ID == OwnerID {
-			s.ChannelMessageSend(m.ChannelID, "You are rich")
-		} else {
-			s.ChannelMessageSend(m.ChannelID, "You are poor")
-		}
-	}
-
-	// send a random image
-	// command: image
-	// reply: random image
-	if m.Content == "image" {
-		url := "https://picsum.photos/200"
-		// send a request to the url
-		resp, err := http.Get(url)
-		if err != nil {
-			s.ChannelMessageSend(m.ChannelID, "Error: "+err.Error())
-			return
-		}
-		// send the image
-		s.ChannelFileSend(m.ChannelID, "image.jpg", resp.Body)
-	}
-
 }
